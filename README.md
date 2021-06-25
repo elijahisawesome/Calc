f998c975ce80977d5597e7ae658f340cd1a8e8fe
elijahisawesome <cheesyman4u2@yahoo.com>
2021-06-25 00:42:50 -0500

    Implemented keyboard support, updated style.

---
+++ README.md
@@ -0,0 +1,3 @@
+Calculator project from a The Odin Project prompt.
+
+Logic for calculator done in a procedural style in JS.
--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const ops = document.querySelectorAll(".ops");
 const evaluate = document.querySelector(".eval");
 const display = document.querySelector(".disp");
 const AC = document.querySelector('.clear');
-
-
+const subDisplay = document.querySelector(".subDisp");
+const backspace = document.querySelector(".backspace");
 
 let num1 = '';
 let num2 = '';
@@ -12,12 +12,10 @@ let runningTotal = 0;
 let curOp = null;
 let prevButton = '';
 
-
-
-
+backspace.addEventListener('click', deleteChars);
 
 nums.forEach(num =>{
-num.addEventListener('click', queueNum);
+num.addEventListener('click', preQueueNum);
 });
 
 ops.forEach(op =>{
@@ -27,45 +25,59 @@ ops.forEach(op =>{
 evaluate.addEventListener('click', operate);
 AC.addEventListener('click', clear);
 
+function deleteChars(){
+    switch (prevButton){
+        case "op": 
+            curOp = null;
+            break;
+        case "number1":
+            num1 = "";
+            break;
+        case "number2":
+            num2 = "";
+            break;
+        case "=":
+            return;
+        default:
+            break;
+    }
+    display.value = "";
+
+}
 
 function add(a,b){
-    runningTotal = a+b;
-    display.value=`${runningTotal}`;
+    runningTotal = parseFloat(a)+parseFloat(b);
 }
 function subtract(a,b){
     runningTotal = a-b;
-    display.value=`${runningTotal}`;
 }
 function multiply(a,b){
     runningTotal = a*b;
-    display.value=`${runningTotal}`;
 }
 function divide(a,b){
+    if(b == "0"){
+        display.value = "Error!";
+        return;
+    }
     runningTotal = (a/b);
-    display.value=`${runningTotal.toString().substring(0,9)}`;
 }
 
-function processor(){
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    let len1 = precision(num1);
-    let len2 = precision(num2);
-    let len;
-
-    if(len1>len2 ? len = len1:len = len2);
-    
-    return len+1;
+document.addEventListener('keypress', logKey);
+function logKey(e){
+    if((parseInt(e.charCode) >= 48 && parseInt(e.charCode) <= 57) || e.key == "."){
+        queueNum(e.key);
+    }
+    else if (e.key == "/" || e.key == "-" || e.key == "+" || e.key == "*"){
+        queueOp(e.key);
+    }
+    else if(e.key == "Enter"){
+        operate();
+    }
 }
-
-function precision(a) {
-    var e = 1;
-    while (Math.round(a * e) / e !== a) e *= 10;
-    return Math.log(e) / Math.LN10;
-  }
 
 function operate(){
-    if(num2){
-        processor();
+    if(num2 !== ""){
+
         switch(curOp){
         
         case "+":
@@ -81,27 +93,32 @@ function operate(){
             divide(num1, num2);
             break;
         default:
-            console.log("Invalid Operator");
-            clear();
+            display.value = "Invalid Operator";
+            curOp = null;
+            return;
     }
-
+    subDisplay.value=`${runningTotal}`;
+    display.value=`${runningTotal}`;
     curOp = null;
     num1 = "";
     num2 = "";
     prevButton = "=";
     }
 }
+function preQueueNum(e){
+    queueNum(e.target.innerText);
+}
 
-function queueNum(e){
-    let thisNum = e.target.innerText;
+function queueNum(thisNum){
+    //let thisNum = e.target.innerText;
 
-    
     if(curOp == null){
         if (thisNum == "." && num1.includes(thisNum)){
             return;
         }
         num1 += thisNum;
         display.value = `${num1}`;
+        prevButton = "number1";
     }
     else{
         if(thisNum == "." && num2.includes(thisNum)){
@@ -109,34 +126,64 @@ function queueNum(e){
         } 
         num2 += thisNum;
         display.value = `${num2}`;
+        prevButton = "number2";
     }
-    prevButton = "number";
+}
+
+function negate(num){
+
+    if(num.includes("-")){
+        return num.toString().slice(1);
+    }
+    return ("-"+num.toString());
+    
+}
+
+function preQueueOp(e){
+    queueOp(e.target.innerText);
 }
 
 function queueOp(e){
+    //negator
+    if(e == "+/-"){
+        if(prevButton == "number1"){
+            num1 = negate(num1);
+            display.value = `${num1}`;
+        }
+        else if(prevButton =="number2"){
+            num2 = negate(num2);
+            display.value = `${num2}`;
+        }
+        else if(prevButton =="="){
+            runningTotal*= -1;
+            display.value = `${runningTotal}`;
+        }
+
+        return;
+    }
+    //all other operation logic
     if(num1 != '' && num2 != ''){
         operate();
         num1 = runningTotal;
-        curOp = e.target.innerText;
+        curOp = e;
         display.value=`${curOp}`;
     }
 
-    else if(num1 != '' && prevButton == "number"){
-        curOp = e.target.innerText;
+    else if(num1 != '' && prevButton.includes("number")){
+        curOp = e;
         display.value=`${curOp}`;
     }
     else if(prevButton == "="){
         num1 = runningTotal;
-        curOp = e.target.innerText;
+        curOp = e;
         display.value=`${curOp}`;
     }
-    else if(prevButton == "number"){
-        curOp = e.target.innerText;
+    else if(prevButton.includes("number")){
+        curOp = e;
         display.value=`${curOp}`;
     }
     prevbutton = "op";
 }
-
 
 function clear(){
     num1 = '';
@@ -144,5 +191,6 @@ function clear(){
     num2 = '';
     curOp = null;
     display.value = '';
+    subDisplay.value = '';
     prevButton = '';
 }
